2ecb6f4b006b60f00c929743d49e5267f7662d2b
jjones <jjones@dkmac-72.local>
2009-01-28 13:37:19 -0600

    added lots of new options to in_place_edit_for to allow behavior overrides

--- lib/in_place_editing.rb
+++ lib/in_place_editing.rb
@@ -15,10 +15,30 @@ module InPlaceEditing
   #
   module ClassMethods
     def in_place_edit_for(object, attribute, options = {})
+      options = {
+                  :object_getter_method => nil,
+                  :object_finder_method => :find,
+                  :object_finder_param => :id,
+                  :attribute_formatter_method => :to_s,
+                  :object_attribute_formatter_method => nil,
+                  :helper_formatter_method => nil
+                }.merge!(options)
       define_method("set_#{object}_#{attribute}") do
-        @item = object.to_s.camelize.constantize.find(params[:id])
+        @item = if options[:object_getter_method]
+          self.send(options[:object_getter_method])
+          self.instance_variable_get("@#{object.to_s}}")
+        else
+          object.to_s.camelize.constantize.send(options[:object_finder_method], params[options[:object_finder_param]])
+        end
         @item.update_attributes(attribute => params[:value])
-        render :text => @item.reload.send(attribute).to_s
+        @item.reload
+        text = if options[:object_attribute_formatter_method]
+          @item.send(options[:object_attribute_formatter_method])
+        else
+          @item.send(attribute).send(options[:attribute_formatter_method])
+        end
+        text = self.send(options[:helper_formatter_method], text) if options[:helper_formatter_method]
+        render :text => text
       end
     end
   end
