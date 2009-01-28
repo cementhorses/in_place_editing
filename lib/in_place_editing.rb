e9a98bf2552434da5fc00397344913b18403e870
jjones <jjones@dkmac-72.local>
2009-01-28 14:11:44 -0600

    shortened option names and made the helper_formatter use the template

--- lib/in_place_editing.rb
+++ lib/in_place_editing.rb
@@ -16,28 +16,28 @@ module InPlaceEditing
   module ClassMethods
     def in_place_edit_for(object, attribute, options = {})
       options = {
-                  :object_getter_method => nil,
-                  :object_finder_method => :find,
+                  :object_getter => nil,
+                  :object_finder => :find,
                   :object_finder_param => :id,
-                  :attribute_formatter_method => :to_s,
-                  :object_attribute_formatter_method => nil,
-                  :helper_formatter_method => nil
+                  :attribute_formatter => :to_s,
+                  :object_attribute_formatter => nil,
+                  :helper_formatter => nil
                 }.merge!(options)
       define_method("set_#{object}_#{attribute}") do
-        @item = if options[:object_getter_method]
-          self.send(options[:object_getter_method])
+        @item = if options[:object_getter]
+          self.send(options[:object_getter])
           self.instance_variable_get("@#{object.to_s}}")
         else
-          object.to_s.camelize.constantize.send(options[:object_finder_method], params[options[:object_finder_param]])
+          object.to_s.camelize.constantize.send(options[:object_finder], params[options[:object_finder_param]])
         end
         @item.update_attributes(attribute => params[:value])
         @item.reload
-        text = if options[:object_attribute_formatter_method]
-          @item.send(options[:object_attribute_formatter_method])
+        text = if options[:object_attribute_formatter]
+          @item.send(options[:object_attribute_formatter])
         else
-          @item.send(attribute).send(options[:attribute_formatter_method])
+          @item.send(attribute).send(options[:attribute_formatter])
         end
-        text = self.send(options[:helper_formatter_method], text) if options[:helper_formatter_method]
+        text = @template.send(options[:helper_formatter], text) if options[:helper_formatter]
         render :text => text
       end
     end
